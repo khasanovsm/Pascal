08ca46c165391abf5a5e4c09e4c9724687a4d3b9
Shamil Khasanov <khasanovsm@Shamils-MacBook-Pro.local>
2019-11-26 12:15:09 +0300

    bts-search- function added

--- CS_MSU/BTS/bts.pas
+++ CS_MSU/BTS/bts.pas
@@ -1,4 +1,20 @@
 program bts;
+
+Type 
+    NodePtr = ^Node;
+    Node = record
+        value : Integer;
+        next : NodePtr;
+    end;
+
+Var 
+    tree : NodePtr;
+
+function search(value : Integer; tree: NodePtr): NodePtr;
+begin
+  
+end;
+
 begin
   
 end.
