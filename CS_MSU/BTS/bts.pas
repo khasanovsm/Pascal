fc3325b888c71feea3b17594e9f6c1923c46565a
Shamil Khasanov <32849207+KhasanovShamil@users.noreply.github.com>
2019-11-26 12:39:22 +0300

    addNode function added

--- CS_MSU/BTS/bts.pas
+++ CS_MSU/BTS/bts.pas
@@ -10,9 +10,20 @@ Type
 Var 
     tree : NodePtr;
 
-function search(value : Integer; tree: NodePtr): NodePtr;
+function addNode(value : Integer; tree: NodePtr): NodePtr;
 begin
-  
+    if tree = nil then
+    begin
+        new(tree);
+        tree^.value := value;
+        tree^.left := nil;
+        tree^.right := nil;
+    end
+    else
+    begin
+        if (value > tree^.value) then addNode(value, tree^.right)
+        else if (value < tree^.value) then addNode(value,tree^.left);
+    end;
 end;
 
 begin
