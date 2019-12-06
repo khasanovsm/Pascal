344659430121e91bdb9edd5d597c01b95291bba5
Shamil Khasanov <khasanovsm@Shamils-MacBook-Pro.local>
2019-12-06 14:25:43 +0300

    new version

--- CS_MSU/semester_proj/backup.pas
+++ CS_MSU/semester_proj/backup.pas
@@ -125,6 +125,132 @@ begin
     for i := 0 to High(reverse) do WriteLn(reverse[i]^.cost.int);
 end;
 
+(*
+function CoctailSort(var productsArray: darray_of_products): performance_parameters;
+var 
+    leftIndex, rightIndex, i: UInt32;
+    tmpElement : RChocolate;
+begin
+    CoctailSort[1] := 0;
+    CoctailSort[2] := 0;
+    leftIndex := 0;
+    rightIndex := High(productsArray);
+    {$B-}
+    while (leftIndex < rightIndex) and (leftIndex <> rightIndex) do 
+    begin   
+        for i := leftIndex to rightIndex - 1 do
+        begin
+            CoctailSort[1] := CoctailSort[1] + 1;
+            if (productsArray[i]^.cost.int > productsArray[i + 1]^.cost.int) 
+            or ((productsArray[i]^.cost.int = productsArray[i + 1]^.cost.int) and (productsArray[i]^.cost.dec > productsArray[i + 1]^.cost.dec)) then begin
+                CoctailSort[2] := CoctailSort[2] + 1;
+                tmpElement := productsArray[i + 1];
+                productsArray[i + 1] := productsArray[i];
+                productsArray[i] := tmpElement;
+            end;
+        end;
+
+        for i := rightIndex - 1 downto leftIndex + 1 do
+        begin
+            CoctailSort[1] := CoctailSort[1] + 1;
+            if (productsArray[i]^.cost.int < productsArray[i - 1]^.cost.int)
+            or ((productsArray[i]^.cost.int = productsArray[i - 1]^.cost.int) and (productsArray[i]^.cost.dec < productsArray[i - 1]^.cost.dec)) then begin
+                CoctailSort[2] := CoctailSort[2] + 1;
+                tmpElement := productsArray[i - 1];
+                productsArray[i - 1] := productsArray[i];
+                productsArray[i] := tmpElement;
+            end;
+        end;
+        leftIndex := leftIndex + 1;
+        rightIndex := rightIndex - 1;
+        //for i := 0 to High(productsArray) do WriteLn(productsArray[i].cost.int, ' ', productsArray[i].cost.dec);
+    end;
+    {$B+}
+end;
+
+function Merge(var left : darray_of_products; var right : darray_of_products; var init : darray_of_products) : performance_parameters;
+var i, j, k: UInt32;
+begin
+    i := 0;
+    j := 0;
+    k := 0;
+    {$B-}
+    while (j < Length(left) ) and (k < Length(right)) do
+    begin
+        if (left[j]^.cost.int < right[k]^.cost.int) or ((left[j]^.cost.int = right[k]^.cost.int) and (left[j]^.cost.dec < right[k]^.cost.dec)) then begin
+            init[i] := left[j];
+            j := j + 1;
+        end
+        else begin
+            init[i] := right[k];
+            k := k + 1;
+        end;
+        i := i + 1;
+        Merge[1] := Merge[1] + 1;
+        Merge[2] := Merge[2] + 1;
+    end;
+    while (j < Length(left)) do
+    begin
+        init[i] := left[j];
+        j := j + 1;
+        i := i + 1;  
+
+        Merge[2] := Merge[2] + 1;
+    end;
+    while (k < Length(right)) do
+    begin
+        init[i] := right[k];
+        k := k + 1;
+        i := i + 1;  
+
+        Merge[2] := Merge[2] + 1;
+    end;
+    {$B+}
+end;
+
+procedure MergeSort(var productsArray : darray_of_products);
+var left, right : darray_of_products;
+    i,j : UInt32;
+begin
+    if Length(productsArray) < 2 then exit;
+    
+    SetLength(left, trunc((Length(productsArray)) / 2));
+    SetLength(right, Length(productsArray) - Length(left));
+    for i := 0 to High(left) do left[i] := productsArray[i];
+    for j := 0 to High(right) do right[j] := productsArray[i + j + 1];
+    //for i := 0 to High(left) do WriteLn('left[',i,'] = ', left[i]^.cost.int);
+    //for i := 0 to High(right) do WriteLn('right[',i,'] = ', right[i]^.cost.int);
+    MergeSort(left);
+    MergeSort(right);
+    //for i := 0 to High(left) do WriteLn('left[',i,'] = ', left[i]^.cost.int);
+    //for i := 0 to High(right) do WriteLn('right[',i,'] = ', right[i]^.cost.int);
+    Merge(left, right, productsArray);
+    //WriteLn('Merged');
+    //for i := 0 to High(left) do WriteLn('left[',i,'] = ', left[i]^.cost.int);
+    //for i := 0 to High(right) do WriteLn('right[',i,'] = ', right[i]^.cost.int);
+    //for i := 0 to High(productsArray) do WriteLn('products[',i,'] = ', productsArray[i]^.cost.int);
+end;
+
+procedure CreateInputDataFile(const products : darray_of_products; path : String);
+var f : TextFile;
+    i : UInt32 = 0;
+    price : Double;
+begin
+    AssignFile(f, path);
+    Rewrite(f);
+    while i < High(productsArray) do begin
+        WriteLn(f,'Milk');
+        WriteLn(f,'Lindt');
+        price := products[i]^.cost.int + products[i]^.cost.dec / 100;
+        WriteLn(f,price:2:2);
+        WriteLn(f,'---');
+        i := i + 1;
+    end;
+    CloseFile(f);
+end;
+
+*)
+
 begin
   
 end.
--- CS_MSU/semester_proj/semester_proj.pas
+++ CS_MSU/semester_proj/semester_proj.pas
@@ -13,172 +13,130 @@ Type
     end;
 
     RChocolate = ^Chocolate;
-    darray_of_products = array of RChocolate;
 
-    performance_parameters = array [1..2] of UInt64;
+    //Chocolate element as a list object
+    RProduct = ^Product; 
+    Product = record
+        item : RChocolate; 
+        next, prev : RProduct;
+    end;
+    //double-linked array
+    DLList = record
+        header, tail : RProduct;
+        length : UInt32;
+    end;
 
 Var
-    productsArray : darray_of_products;
+    list_of_products : DLList;
+    elem : RProduct;
 
-    i : UInt32;
-    pp : performance_parameters;
+    comparisons, permutations : UInt64;
 
-    FromTime, ToTime: TDateTime;
-    DiffMinutes: Integer;
-
-procedure initializeProductsArray(var productsArray : darray_of_products; length : UInt32);
+procedure initializeProducts(var products : DLList; file_path : String);
 var 
-    i : UInt32;
+    f : TextFile;
+    price : Real;
+    cur, prev : RProduct; // identical to products.header (./tail)
 begin
-    SetLength(productsArray,Length);
-    for i := 0 to High(productsArray) do 
+    //file handling
+    AssignFile(f, file_path);
+    Reset(f);
+
+    New(products.header); // создается новый объект типа Pruduct
+    New(products.tail);  //создается новый объект типа Pruduct
+    products.length := 0;
+
+    cur := products.header;
+    prev := nil;
+
+    while not EOF(f) do
     begin
-        New(productsArray[i]);
-        productsArray[i]^.cost.int := Random(200)  + 36;
-        productsArray[i]^.cost.dec := Random(100);
+        products.length := products.length + 1;
+        New(cur^.item);
+        ReadLn(f,cur^.item^.kind);
+        Readln(f,cur^.item^.producer);
+        ReadLn(f, price);
+        ReadLn(f);
+        cur^.item^.cost.int := trunc(price);
+        cur^.item^.cost.dec := trunc((price - trunc(price))*100);
+        if prev <> Nil then cur^.prev := prev;
+        prev := cur;
+        New(cur^.next);
+        cur := cur^.next;
     end;
+    products.tail := prev;
+    Dispose(products.tail^.next);
+    products.tail^.next := Nil;
+    CloseFile(f);
 end;
 
-
-function CoctailSort(var productsArray: darray_of_products): performance_parameters;
+procedure CoctailSort(var products: DLList);
 var 
-    leftIndex, rightIndex, i: UInt32;
+    left, right, i: RProduct;
     tmpElement : RChocolate;
+    swapped : Boolean = True;
 begin
-    CoctailSort[1] := 0;
-    CoctailSort[2] := 0;
-    leftIndex := 0;
-    rightIndex := High(productsArray);
-    {$B-}
-    while (leftIndex < rightIndex) and (leftIndex <> rightIndex) do 
-    begin   
-        for i := leftIndex to rightIndex - 1 do
-        begin
-            CoctailSort[1] := CoctailSort[1] + 1;
-            if (productsArray[i]^.cost.int > productsArray[i + 1]^.cost.int) 
-            or ((productsArray[i]^.cost.int = productsArray[i + 1]^.cost.int) and (productsArray[i]^.cost.dec > productsArray[i + 1]^.cost.dec)) then begin
-                CoctailSort[2] := CoctailSort[2] + 1;
-                tmpElement := productsArray[i + 1];
-                productsArray[i + 1] := productsArray[i];
-                productsArray[i] := tmpElement;
-            end;
-        end;
+    //side effects 
+    comparisons := 0;
+    permutations := 0;
 
-        for i := rightIndex - 1 downto leftIndex + 1 do
+    left := products.header;
+    right := products.tail^.prev;
+    {$B-}
+    while swapped do 
+    begin   
+        swapped := False;
+        i := left;
+        while i <> right^.next do
         begin
-            CoctailSort[1] := CoctailSort[1] + 1;
-            if (productsArray[i]^.cost.int < productsArray[i - 1]^.cost.int)
-            or ((productsArray[i]^.cost.int = productsArray[i + 1]^.cost.int) and (productsArray[i]^.cost.dec < productsArray[i + 1]^.cost.dec)) then begin
-                CoctailSort[2] := CoctailSort[2] + 1;
-                tmpElement := productsArray[i - 1];
-                productsArray[i - 1] := productsArray[i];
-                productsArray[i] := tmpElement;
+            comparisons := comparisons + 1;
+            if (i^.item^.cost.int > i^.next^.item^.cost.int) 
+                or ((i^.item^.cost.int =  i^.next^.item^.cost.int) and (i^.item^.cost.dec >  i^.next^.item^.cost.dec)) then begin
+                tmpElement := i^.next^.item;
+                i^.next^.item := i^.item;
+                i^.item := tmpElement;
+                swapped := True;
+                permutations := permutations + 1;
             end;
+            i := i^.next;
         end;
-        leftIndex := leftIndex + 1;
-        rightIndex := rightIndex - 1;
+        //if nothing moved, then array is sorted
+        if not swapped then Break;
+
+        //otherwise, reset the swapped flag so that it can be used in the next stage
+        swapped := False;
+
+        i := right;
+        while i <> left do
+        begin
+            comparisons := comparisons + 1;
+            if (i^.item^.cost.int < i^.prev^.item^.cost.int)
+                or ((i^.item^.cost.int = i^.prev^.item^.cost.int) and (i^.item^.cost.dec < i^.prev^.item^.cost.dec)) then begin
+                tmpElement := i^.prev^.item;
+                i^.prev^.item := i^.item;
+                i^.item := tmpElement;
+                swapped := True;
+                permutations := permutations + 1;
+            end;
+            i := i^.prev;
+        end;
+        left := left^.next;
+        right := right^.prev;
         //for i := 0 to High(productsArray) do WriteLn(productsArray[i].cost.int, ' ', productsArray[i].cost.dec);
     end;
     {$B+}
 end;
 
-function Merge(var left : darray_of_products; var right : darray_of_products; var init : darray_of_products) : performance_parameters;
-var i, j, k: UInt32;
 begin
-    i := 0;
-    j := 0;
-    k := 0;
-    {$B-}
-    while (j < Length(left) ) and (k < Length(right)) do
+    initializeProducts(list_of_products, '/Users/khasanovsm/OneDrive/Programming/Pascal/CS_MSU/semester_proj/input_data/raw_input_data.txt');
+    CoctailSort(list_of_products);
+    WriteLn(comparisons,' ', permutations);
+    elem := list_of_products.header;
+    while True do
     begin
-        if (left[j]^.cost.int < right[k]^.cost.int) or ((left[j]^.cost.int = right[k]^.cost.int) and (left[j]^.cost.dec < right[k]^.cost.dec)) then begin
-            init[i] := left[j];
-            j := j + 1;
-        end
-        else begin
-            init[i] := right[k];
-            k := k + 1;
-        end;
-        i := i + 1;
-        Merge[1] := Merge[1] + 1;
-        Merge[2] := Merge[2] + 1;
+        WriteLn(elem^.item^.cost.int, ' ', elem^.item^.cost.dec);
+        if elem^.next <> nil then elem := elem^.next
+        else Break;
     end;
-    while (j < Length(left)) do
-    begin
-        init[i] := left[j];
-        j := j + 1;
-        i := i + 1;  
-
-        Merge[2] := Merge[2] + 1;
-    end;
-    while (k < Length(right)) do
-    begin
-        init[i] := right[k];
-        k := k + 1;
-        i := i + 1;  
-
-        Merge[2] := Merge[2] + 1;
-    end;
-    {$B+}
-end;
-
-procedure MergeSort(var productsArray : darray_of_products);
-var left, right : darray_of_products;
-    i,j : UInt32;
-begin
-    if Length(productsArray) < 2 then exit;
-    
-    SetLength(left, trunc((Length(productsArray)) / 2));
-    SetLength(right, Length(productsArray) - Length(left));
-    for i := 0 to High(left) do left[i] := productsArray[i];
-    for j := 0 to High(right) do right[j] := productsArray[i + j + 1];
-    //for i := 0 to High(left) do WriteLn('left[',i,'] = ', left[i]^.cost.int);
-    //for i := 0 to High(right) do WriteLn('right[',i,'] = ', right[i]^.cost.int);
-    MergeSort(left);
-    MergeSort(right);
-    //for i := 0 to High(left) do WriteLn('left[',i,'] = ', left[i]^.cost.int);
-    //for i := 0 to High(right) do WriteLn('right[',i,'] = ', right[i]^.cost.int);
-    Merge(left, right, productsArray);
-    //WriteLn('Merged');
-    //for i := 0 to High(left) do WriteLn('left[',i,'] = ', left[i]^.cost.int);
-    //for i := 0 to High(right) do WriteLn('right[',i,'] = ', right[i]^.cost.int);
-    //for i := 0 to High(productsArray) do WriteLn('products[',i,'] = ', productsArray[i]^.cost.int);
-end;
-
-procedure CreateInputDataFile(const products : darray_of_products; path : String);
-var f : TextFile;
-    i : UInt32 = 0;
-    price : Double;
-begin
-    AssignFile(f, path);
-    Rewrite(f);
-    while i < High(productsArray) do begin
-        WriteLn(f,'Milk');
-        WriteLn(f,'Lindt');
-        price := products[i]^.cost.int + products[i]^.cost.dec / 100;
-        WriteLn(f,price:2:2);
-        WriteLn(f,'---');
-        i := i + 1;
-    end;
-    CloseFile(f);
-end;
-
-begin
-    initializeProductsArray(productsArray, 100);
-    //for i := 0 to High(productsArray) do WriteLn(productsArray[i]^.cost.int, ' ', productsArray[i]^.cost.dec);
-    //pp := CoctailSort(productsArray);
-    //Writeln(pp[1], ' ', pp[2]);
-    //for i := 0 to High(productsArray) do WriteLn(productsArray[i].cost.int, ' ', productsArray[i].cost.dec);
-    //(*
-    FromTime := Now;
-    //MergeSort(productsArray);
-    //CoctailSort(productsArray);
-    ToTime := Now;
-    DiffMinutes := MilliSecondsBetween(ToTime,FromTime);
-    WriteLn(DiffMinutes);
-    //*)
-    CreateInputDataFile(productsArray,'/Users/khasanovsm/OneDrive/Programming/Pascal/CS_MSU/semester_proj/raw_input_data.txt');
-    //WriteLn('Sorted');
-    //for i := 0 to High(productsArray) do WriteLn(productsArray[i]^.cost.int, ' ', productsArray[i]^.cost.dec);
-    //EssentialMergeSort(productsArray);
+    //WriteLn(list_of_products.tail^.item^.cost.int, ' ', list_of_products.tail^.item^.cost.dec);
 end.
